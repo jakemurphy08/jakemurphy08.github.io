754ea2983690ce0d2af444d644dd760ffe8f15e6
jakemurphy08 <71108469+jakemurphy08@users.noreply.github.com>
2021-04-07 18:46:20 -0400

    Update pngConverter.py

--- pngConverter.py
+++ pngConverter.py
@@ -1,1 +1,21 @@
-image = Image.open('RGB.png')
+# Jake Murphy
+# April 6, 2021
+
+import glob
+
+# path to the image file
+#path = '/Users/jakemurphy/Downloads/Interview Problem/RGB.png'
+
+# opening the image file
+#img = open(path,'rb')
+
+#for line in img:
+#    print(line)
+
+# path to the image file
+imagePath = '/Users/jakemurphy/Downloads/Interview Problem/RGB.png'
+
+
+for imagePath in glob.glob('/Users/jakemurphy/Downloads/Interview Problem/*.png'):
+    image = (imagePath)
+    print(imagePath)
